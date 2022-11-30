9d62471db0376f2fb9b35a056f2a580cdb5655c0
AndreyKarabut <karabut.andrey2018@yandex.ru>
2022-11-30 20:25:32 +0600

    function changes HP hero and Being

--- src/main/java/Cards/Being.java
+++ src/main/java/Cards/Being.java
@@ -2,4 +2,67 @@ package Cards;
 
 public class Being extends Card{
 
+    int currentHp;
+    int buffHp;
+    int startHp;
+    int receivedDamage;
+    int maxHp;
+
+    int currentAttack;
+    int buffAttack;
+    int startAttack;
+
+    public void setMaxHp(){
+        this.maxHp = this.startHp + this.buffHp;
+    }
+
+    public void setBuffHp(int plusbuffHp) {
+        this.buffHp = this.buffHp + plusbuffHp;
+        setCurrentHp(0 , plusbuffHp,0);
+        setMaxHp();
+    }
+
+    public void setReceivedDamage(int plusReceivedDamage) {
+        this.receivedDamage = this.receivedDamage + plusReceivedDamage;
+        setCurrentHp(plusReceivedDamage, 0, 0);
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    public void setCurrentHp(int damage, int buff, int heal) {
+        if (damage > 0){
+            this.currentHp = this.currentHp - damage;
+        }
+        if (buff > 0){
+            this.currentHp = this.currentHp + buff;
+        }
+        if (heal > 0){
+            if (this.currentHp + heal >= this.maxHp){
+                this.currentHp = this.maxHp;
+            } else {
+                this.currentHp = this.currentHp + heal;
+            }
+        }
+    }
+
+
+    public int getCurrentAttack() {
+        return currentAttack;
+    }
+
+    public void setCurrentAttack(int currentAttack) {
+        this.currentAttack = currentAttack;
+    }
+
+    public void dealDamageBeing(Being dealer, Being ricipient){
+        ricipient.setReceivedDamage(dealer.getCurrentAttack());
+        dealer.setReceivedDamage(ricipient.getCurrentAttack());
+    }
+
+    public boolean iAmDead(Being being){
+        return being.currentHp < 1;
+    }
+
 }
--- src/main/java/Player/Hero.java
+++ src/main/java/Player/Hero.java
@@ -1,4 +1,68 @@
 package Player;
 
 public class Hero {
+    int currentHp;
+    int buffHp;
+    int startHp;
+    int receivedDamage;
+    int maxHp;
+
+    int currentArmor;
+
+    int currentAttack;
+    int buffAttack;
+    int startAttack;
+
+    public void setMaxHp(){
+        this.maxHp = this.startHp + this.buffHp;
+    }
+
+    public void setBuffHp(int plusbuffHp) {
+        this.buffHp = this.buffHp + plusbuffHp;
+        setCurrentHp(0, plusbuffHp, 0);
+        setMaxHp();
+    }
+
+    public void setReceivedDamage(int plusReceivedDamage) {
+        this.receivedDamage = this.receivedDamage + plusReceivedDamage;
+        setCurrentHp(plusReceivedDamage, 0, 0);
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    private void setCurrentHp(int damage, int buffHp, int healHp) {
+        if (damage > 0) {
+            if (currentArmor > 0) {
+                setCurrentArmor(0, damage);
+            }
+            this.currentHp = this.currentHp - damage;
+        }
+        if (buffHp > 0) {
+            this.currentHp = this.currentHp + buffHp;
+        }
+        if (healHp > 0){
+            if (this.currentHp + healHp >= this.maxHp){
+                this.currentHp = this.maxHp;
+            } else {
+                this.currentHp = this.currentHp + healHp;
+            }
+        }
+    }
+
+    public void setCurrentArmor(int buffArmor, int damage){
+        int extraDamage = this.currentArmor - damage;
+        this.currentArmor = this.currentArmor + buffArmor - damage;
+        setCurrentHp(extraDamage, 0, 0);
+    }
+
+    public void healMySelf(int healHp){
+        setCurrentHp(0, 0 , healHp);
+    }
+
+    public boolean iAmDead(){
+        return this.currentHp < 1;
+    }
+
 }
